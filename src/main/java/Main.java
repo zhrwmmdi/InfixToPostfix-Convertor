c34222f1c61b0ed4528984c85fdc5f733fcfe596
zhrwmmdi <zahra.mmdi2003@gmail.com>
2023-12-17 13:11:45 +0330

    to postfix conversion and calculation of postfix expression done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,11 @@ public class Main {
         InfixToPostfix infixToPostfix = new InfixToPostfix();
 
 
-        //String n1 = "A+B*(C-D)";
-        String n1 = "A*B/C+D-E*F";
+        String n1 = "3+(4*8)";
+        //String n1 = "3+4*8";
         List<String> g = infixToPostfix.inputToList(n1);
         List<String> p = infixToPostfix.toPostFix(g);
-        System.out.println(p);
-
-
-
-
-
-
+        System.out.println(infixToPostfix.calculator(p));
 
     }
 }
